ff858847ab75fd2416124047a8ef0a12138468aa
Lukas Stabe <lukas@stabe.de>
2020-09-26 23:16:20 +0200

    cargo fmt

--- build.rs
+++ build.rs
@@ -7,7 +7,12 @@ fn main() {
     let dest_path = Path::new(&out_dir).join("style.css");
     fs::write(
         &dest_path,
-        grass::from_string(include_str!("data/style.scss").to_string(), &grass::Options::default()).unwrap()
-    ).unwrap();
+        grass::from_string(
+            include_str!("data/style.scss").to_string(),
+            &grass::Options::default(),
+        )
+        .unwrap(),
+    )
+    .unwrap();
     println!("cargo:rerun-if-changed=data/style.css");
 }
