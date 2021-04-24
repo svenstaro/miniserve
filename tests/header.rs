dfd0ecf931b68ea373be1e4e785421d48ca6fed5
Ali MJ Al-Nasrawy <alimjalnasrawy@gmail.com>
2021-04-24 07:28:55 +0300

    tests: Refactor!
    Remove duplicate code responsible for the initial setup and teardown of
    the test binary.
    
    This introduces `TestServer` as a resource manager for a running
    miniserve binary, which can be created with the fixtures `server()` and
    `server_no_stderr()`
    
    It also provides convenience function for handling server url.

--- tests/header.rs
+++ tests/header.rs
@@ -1,29 +1,15 @@
 mod fixtures;
 
-use assert_cmd::prelude::*;
-use assert_fs::fixture::TempDir;
-use fixtures::{port, tmpdir, Error};
+use fixtures::{server, Error};
 use rstest::rstest;
-use std::process::{Command, Stdio};
-use std::thread::sleep;
-use std::time::Duration;
 
 #[rstest(headers,
     case(vec!["x-info: 123".to_string()]),
     case(vec!["x-info1: 123".to_string(), "x-info2: 345".to_string()])
 )]
-fn custom_header_set(tmpdir: TempDir, port: u16, headers: Vec<String>) -> Result<(), Error> {
-    let mut child = Command::cargo_bin("miniserve")?
-        .arg(tmpdir.path())
-        .arg("-p")
-        .arg(port.to_string())
-        .args(headers.iter().flat_map(|h| vec!["--header", h]))
-        .stdout(Stdio::null())
-        .spawn()?;
-
-    sleep(Duration::from_secs(1));
-
-    let resp = reqwest::blocking::get(format!("http://localhost:{}", port).as_str())?;
+fn custom_header_set(headers: Vec<String>) -> Result<(), Error> {
+    let server = server(headers.iter().flat_map(|h| vec!["--header", h]));
+    let resp = reqwest::blocking::get(server.url())?;
 
     for header in headers {
         let mut header_split = header.splitn(2, ':');
@@ -31,8 +17,6 @@ fn custom_header_set(tmpdir: TempDir, port: u16, headers: Vec<String>) -> Result
         let header_value = header_split.next().unwrap().trim();
         assert_eq!(resp.headers().get(header_name).unwrap(), header_value);
     }
-
-    child.kill()?;
 
     Ok(())
 }
