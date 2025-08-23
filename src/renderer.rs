b5a08520bd3002322b8ef4763abebc4e10116c08
7sDream <i@7sdre.am>
2025-08-23 20:23:25 +0800

    style: run linter

--- src/renderer.rs
+++ src/renderer.rs
@@ -561,10 +561,10 @@ fn build_link(
 
     query_items.push(format!("sort={name}&order={order}"));
 
-    if let Some(search) = search {
-        if !search.is_empty() {
-            query_items.push(format!("search={}", utf8_percent_encode(search, COMPONENT)));
-        }
+    if let Some(search) = search
+        && !search.is_empty()
+    {
+        query_items.push(format!("search={}", utf8_percent_encode(search, COMPONENT)));
     }
 
     let link = format!("?{}", query_items.join("&"));
