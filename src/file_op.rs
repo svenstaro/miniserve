615d51a893e5a84b806cbc4d42ba1844321a2cd4
cjc <decsoily@outlook.com>
2025-10-24 15:07:45 +0800

    fix: --rm-files-does-not-work-with---route-prefix issue#1524

--- src/file_op.rs
+++ src/file_op.rs
@@ -551,9 +551,17 @@ pub async fn rm_file(
     query: web::Query<FileOpQueryParameters>,
 ) -> Result<HttpResponse, RuntimeError> {
     let conf = req.app_data::<web::Data<MiniserveConfig>>().unwrap();
-    let rm_path = sanitize_path(&query.path, conf.show_hidden).ok_or_else(|| {
+    let mut rm_path = sanitize_path(&query.path, conf.show_hidden).ok_or_else(|| {
         RuntimeError::InvalidPathError("Invalid value for 'path' parameter".to_string())
     })?;
+    println!("{:?}, {:?}", rm_path, conf.route_prefix);
+    if conf.route_prefix != "/" && check_prefix(&rm_path, &conf.route_prefix) {
+        if let Some(rmed_path) = remove_prefix(&rm_path, &conf.route_prefix) {
+            rm_path = rmed_path;
+        }
+    }
+    println!("{:?}", rm_path);
+
     let app_root_dir = conf.path.canonicalize().map_err(|e| {
         RuntimeError::IoError("Failed to resolve path served by miniserve".to_string(), e)
     })?;
@@ -604,4 +612,21 @@ pub async fn rm_file(
     Ok(HttpResponse::SeeOther()
         .append_header((header::LOCATION, return_path))
         .finish())
+}
+
+fn check_prefix(path: &Path, prefix: &str) -> bool {
+    let prefix = prefix.trim_start_matches("/");
+
+    if path.starts_with(prefix) {
+        return true;
+    }
+    false
+}
+
+fn remove_prefix(path: &Path, prefix: &str) -> Option<PathBuf> {
+    let prefix = prefix.trim_start_matches("/");
+
+    path.strip_prefix(prefix)
+        .ok()
+        .map(|stripped_path| stripped_path.to_path_buf())
 }
